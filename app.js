d2a7f43cd341ee2eac7623d66a898454771cb11a
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2022-05-03 14:10:53 -0500

    body styled and fonts imported

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("yes")
+// WEATHER API KEY = e082691f75210af0967a6dd582e4b693
+
+
+
+
+// fetch('https://fakestoreapi.com/products')
+// .then(res=>res.json())
+// .then(json=>console.log(json))
