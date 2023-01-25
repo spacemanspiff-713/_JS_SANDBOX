3e7d025cc2b43f26bd1eea77529252af2a025faf
spacemanspiff-713 <87157301+spacemanspiff-713@users.noreply.github.com>
2023-01-25 10:29:11 -0600

    Knee deep into a Udemy JS Projects Course "https://www.udemy.com/course/javascript-web-projects-to-build-your-portfolio-resume/"

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 // WEATHER API KEY = e082691f75210af0967a6dd582e4b693
-
-
-
+const fruits = ['kiwi','mango','apple','pear'];
+function doubleName(value, index){
+    console.log(`${index + 1} ${value+value}`)
+}
+fruits.forEach(doubleName)
 
 // fetch('https://fakestoreapi.com/products')
 // .then(res=>res.json())
 // .then(json=>console.log(json))
+
+// GET QUOTES FROM AN API
+let apiQuotes = [];
+let singleQuote = {};
+// DEFINE QUOTE SELECTION FUNCTION
+
+async function getQuotes(){
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes)
+        newQuote()
+    }catch(error){
+        // CATCH ERROR
+
+    }
+}
+
+function newQuote(){
+    // PICK A RANDOM QUOTE
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    // let quote = apiQuotes[2];
+    console.log(quote)
+}
+
+newQuote()
+getQuotes()
